604fa094f48e531271c181a4638d8ff6a6d633bb
Becky <bjentwistle@gmail.com>
2023-05-01 15:16:40 +0100

    show session details done

--- project_code/controllers/sessions_controller.py
+++ project_code/controllers/sessions_controller.py
@@ -13,3 +13,10 @@ sessions_blueprint = Blueprint("sessions", __name__)
 def show_all_sessions():
     sessions = session_repo.select_all()
     return render_template('/sessions/sessions.jinja', title = "All sessions page", sessions = sessions)
+
+
+@sessions_blueprint.route("/sessions/<id>")
+def show_session_index(id):
+    session = session_repo.select(id)
+    return render_template("sessions/single_session.jinja", session = session)
+
