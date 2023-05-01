1999828e113f3d6287bdbb3b01a36fc9c7db166b
Becky <bjentwistle@gmail.com>
2023-05-01 15:34:01 +0100

    list all members and single member done

--- project_code/controllers/members_controller.py
+++ project_code/controllers/members_controller.py
@@ -8,3 +8,14 @@ import repositories.booking_repo as booking_repo
 import repositories.session_repo as session_repo 
 
 members_blueprint = Blueprint("members", __name__)
+
+@members_blueprint.route("/members")
+def show_all_members():
+    members = member_repo.select_all()
+    return render_template('/members/members.jinja', title = "All members page", members = members)
+
+
+@members_blueprint.route("/members/<id>")
+def show_member_index(id):
+    member = member_repo.select(id)
+    return render_template("members/single_member.jinja", member = member)
